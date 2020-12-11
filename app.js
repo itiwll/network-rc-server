d1e93b555fcfea7a49490b82b39ed81460e234b2
Eson Wong <itiwll@gmail.com>
2020-12-12 00:53:41 +0800

    Update for access control

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/afdian', afdianRouter);
